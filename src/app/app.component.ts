a3ce738be5e9201cb61109d1d601a4e51fe015de
Vignesh <vignesh000129@gmail.com>
2023-08-16 21:36:22 +0530

    Added search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './service/weather.service';
+import { WeatherData } from './common/weather-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weatherApp';
+export class AppComponent implements OnInit {
+
+
+  weatherData: WeatherData | undefined;
+  constructor(private weatherService: WeatherService) {
+
+  }
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('New Delhi').subscribe(
+      {
+        next: (response) => {
+          console.log(response);
+          console.log(response.main);
+          this.weatherData = response;
+        }
+      }
+    );
+  }
 }
--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -1,23 +1,27 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { WeatherData } from '../common/weather-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-
+  XRapidAPIKey: string = 'X-RapidAPI-Key';
+  XRapidAPIKeyValue: string = '49b91184bbmsh58fd53a5a2f6f41p1773a4jsn25e326aac962';
+  XRapidAPIHost: string = 'X-RapidAPI-Host';
+  XRapidAPIHostValue: string = 'open-weather13.p.rapidapi.com';
   // private baseUrl = 'https://open-weather-map27.p.rapidapi.com/weather';
   constructor(private http: HttpClient) { }
 
-  getWeatherData(cityName: string) {
-    
-  const baseWeatherUrl :string= `https://open-weather13.p.rapidapi.com/city/${cityName}`;
-
-    this.http.get(baseWeatherUrl), {
-      header: new HttpHeaders()
-        .set(environment.XRapidAPIKey, environment.XRapidAPIKey)
-        .set(environment.XRapidAPIHost, environment.XRapidAPIHostValue)
-    }
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    const baseWeatherUrl: string = `https://open-weather13.p.rapidapi.com/city/${cityName}`;
+    const headers = new HttpHeaders()
+      .set(this.XRapidAPIKey, this.XRapidAPIKeyValue)
+      .set(this.XRapidAPIHost,this.XRapidAPIHostValue);
+    return this.http.get<WeatherData>(baseWeatherUrl, { headers });
   }
+
 }
+
