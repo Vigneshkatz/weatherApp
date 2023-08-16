ae2c301d6d0f28e2014382125eb5c207e2e39e31
Vignesh <vignesh000129@gmail.com>
2023-08-16 20:33:17 +0530

    Added Model class

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -1,10 +1,23 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  private baseUrl = 'https://open-weather-map27.p.rapidapi.com/weather';
-  constructor() { }
+  // private baseUrl = 'https://open-weather-map27.p.rapidapi.com/weather';
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName: string) {
+    
+  const baseWeatherUrl :string= `https://open-weather13.p.rapidapi.com/city/${cityName}`;
+
+    this.http.get(baseWeatherUrl), {
+      header: new HttpHeaders()
+        .set(environment.XRapidAPIKey, environment.XRapidAPIKey)
+        .set(environment.XRapidAPIHost, environment.XRapidAPIHostValue)
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+    production:false,
+    baseWeatherUrl:'https://open-weather13.p.rapidapi.com/city/landon',
+    XRapidAPIKey:'X-RapidAPI-Key',
+    XRapidAPIKeyValue:'185230787amshbe4d0ab1f233ad7p16289ejsn28db17a9374e',
+    XRapidAPIHost:'X-RapidAPI-Host',
+    XRapidAPIHostValue:'open-weather13.p.rapidapi.com'
+}
